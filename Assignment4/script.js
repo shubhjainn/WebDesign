2ccb42a140b371cbd171ee80f00487320d2b54ba
shubhjainn <jain.shu@northeastern.edu>
2023-02-06 12:47:11 -0500

    added checkboxes and corresponding text fields for all the select values

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -80,38 +80,148 @@ function submitted(e) {
 
 function selectProgramming(lang){
     var selectedLanguage = lang.value
+
+    var cpp_styling =  document.getElementById("c++");
+    var python_styling =  document.getElementById("python");
+    var java_styling =  document.getElementById("java");
+    var js_styling =  document.getElementById("js");
+    var c_hash_styling =  document.getElementById("c#");
+
     if(selectedLanguage == 1){
-        document.getElementById("c++").style.display = "block"; 
+      cpp_styling.style.display = "block"; 
     }
     else{
-        document.getElementById("c++").style.display = "none"; 
+      cpp_styling.style.display = "none"; 
     }
 
     if(selectedLanguage == 2){
-        document.getElementById("python").style.display = "block"; 
+       python_styling.style.display = "block"; 
     }
     else{
-        document.getElementById("python").style.display = "none"; 
+      python_styling.style.display = "none"; 
     }
 
     if(selectedLanguage == 3){
-        document.getElementById("java").style.display = "block"; 
+      java_styling.style.display = "block"; 
     }
     else{
-        document.getElementById("java").style.display = "none"; 
+      java_styling.style.display = "none"; 
     }
 
     if(selectedLanguage == 4){
-        document.getElementById("js").style.display = "block"; 
+      js_styling.style.display = "block"; 
     }
     else{
-        document.getElementById("js").style.display = "none"; 
+      js_styling.style.display = "none"; 
     }
 
     if(selectedLanguage == 5){
-        document.getElementById("c#").style.display = "block"; 
+      c_hash_styling.style.display = "block"; 
     }
     else{
-        document.getElementById("c#").style.display = "none"; 
+      c_hash_styling.style.display = "none"; 
     }
+}
+
+function showTextArea(){
+
+  var cpp_checkbox1 =  document.getElementById("c++_1");
+  var cpp_checkbox2 =  document.getElementById("c++_2");
+
+  var python_checkbox1 =  document.getElementById("python_1");
+  var python_checkbox2 =  document.getElementById("python_2");
+
+  var java_checkbox1 =  document.getElementById("java_1");
+  var java_checkbox2 =  document.getElementById("java_2");
+
+  var js_checkbox1 =  document.getElementById("js_1");
+  var js_checkbox2 =  document.getElementById("js_2");
+
+  var c_hash_checkbox1 =  document.getElementById("c#_1");
+  var c_hash_checkbox2 =  document.getElementById("c#_2");
+
+
+  //2 checkboxes for c++
+    if(cpp_checkbox1.checked == true){
+        document.getElementById("c++_text_1").style.display = "block";
+    }
+    else{
+        document.getElementById("c++_text_1").style.display = "none"; 
+    }
+
+    if(cpp_checkbox2.checked == true){
+      document.getElementById("c++_text_2").style.display = "block";
+    }
+    else{
+      document.getElementById("c++_text_2").style.display = "none"; 
+    }
+
+
+  //2 checkboxes for python
+  if(python_checkbox1.checked == true){
+      document.getElementById("python_text_1").style.display = "block";
+  }
+  else{
+      document.getElementById("python_text_1").style.display = "none"; 
+  }
+
+  if(python_checkbox2.checked == true){
+    document.getElementById("python_text_2").style.display = "block";
+  }
+  else{
+    document.getElementById("python_text_2").style.display = "none"; 
+  }
+
+
+
+    //2 checkboxes for java
+  if(java_checkbox1.checked == true){
+      document.getElementById("java_text_1").style.display = "block";
+  }
+  else{
+      document.getElementById("java_text_1").style.display = "none"; 
+  }
+
+  if(java_checkbox2.checked == true){
+    document.getElementById("java_text_2").style.display = "block";
+  }
+  else{
+    document.getElementById("java_text_2").style.display = "none"; 
+  }
+
+
+
+   //2 checkboxes for js
+   if(c_hash_checkbox1.checked == true){
+    document.getElementById("c#_text_1").style.display = "block";
+  }
+  else{
+    document.getElementById("c#_text_1").style.display = "none"; 
+  }
+
+  if(c_hash_checkbox2.checked == true){
+  document.getElementById("c#_text_2").style.display = "block";
+  }
+  else{
+  document.getElementById("c#_text_2").style.display = "none"; 
+  }
+
+
+
+  //2 checkboxes for c#
+  if(js_checkbox1.checked == true){
+    document.getElementById("js_text_1").style.display = "block";
+  }
+  else{
+    document.getElementById("js_text_1").style.display = "none"; 
+  }
+
+  if(js_checkbox2.checked == true){
+  document.getElementById("js_text_2").style.display = "block";
+  }
+  else{
+  document.getElementById("js_text_2").style.display = "none"; 
+  }
+
+
 }
