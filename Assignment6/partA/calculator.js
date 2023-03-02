8d429e8dd1dfc78674a323bbe1428671ea91cfae
shubhjainn <jain.shu@northeastern.edu>
2023-03-02 15:43:20 -0500

    removed comments and unused lines

--- Assignment6/partA/calculator.js
+++ Assignment6/partA/calculator.js
@@ -4,7 +4,6 @@ document.getElementById("add").addEventListener('click', () =>{
     let input1 = document.querySelector('#input1');
     let input2 = document.querySelector('#input2');
     console.log(input1.value);
-    let result = document.querySelector('h1');
     let total = parseFloat(input1.value) + parseFloat(input2.value);
     console.log(total);
     document.getElementById("output").value=total.toFixed(2);
@@ -14,7 +13,6 @@ document.getElementById("add").addEventListener('click', () =>{
 document.getElementById("subtract").addEventListener('click', () =>{
     let input1 = document.querySelector('#input1');
     let input2 = document.querySelector('#input2');
-    let result = document.querySelector('h1');
     let total = parseFloat(input1.value) - parseFloat(input2.value);
     document.getElementById("output").value=total.toFixed(2);
 });
@@ -22,7 +20,6 @@ document.getElementById("subtract").addEventListener('click', () =>{
 document.getElementById("multiply").addEventListener('click', () =>{
     let input1 = document.querySelector('#input1');
     let input2 = document.querySelector('#input2');
-    let result = document.querySelector('h1');
     let total = parseFloat(input1.value) * parseFloat(input2.value);
     document.getElementById("output").value=total.toFixed(2);
 });
@@ -30,7 +27,6 @@ document.getElementById("multiply").addEventListener('click', () =>{
 document.getElementById("divide").addEventListener('click', () =>{
     let input1 = document.querySelector('#input1');
     let input2 = document.querySelector('#input2');
-    let result = document.querySelector('h1');
     let total = parseFloat(input1.value) / parseFloat(input2.value);
     document.getElementById("output").value=total.toFixed(2);
 });
